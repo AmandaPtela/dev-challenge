2a55c9f58db5726c3a2dd77c7e669cfac2ddfd5c
Amanda Portela <mandaptela@gmail.com>
2024-11-24 19:12:46 -0300

    ref: simple validation for received domain added

--- src/Desafio.Umbler.Front/src/Components/SearchDomain.jsx
+++ src/Desafio.Umbler.Front/src/Components/SearchDomain.jsx
@@ -2,8 +2,15 @@ import React, { useState } from "react";
 
 export default function Search() {
     const [domain, setDomain] = useState("");
+    let [valid, setValid] = useState(true);
 
     function handleValue(value) {
+        const regex = /^(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.(?!-)[A-Za-z0-9-]{2,63}(?<!-)$/
+
+        if (value.length <= 1 || !regex.test(value)) {
+            return setValid(false);
+        };
+        setValid(true);
         setDomain(value);
     }
 
