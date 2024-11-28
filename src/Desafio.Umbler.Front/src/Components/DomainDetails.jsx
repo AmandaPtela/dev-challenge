471a4ed86691a23777cc9a4e3fc29d947f3ad111
Amanda Portela <mandaptela@gmail.com>
2024-11-28 20:41:14 -0300

    feat: initial structure of component used to show domain's informations

--- src/Desafio.Umbler.Front/src/Components/DomainDetails.jsx
+++ src/Desafio.Umbler.Front/src/Components/DomainDetails.jsx
@@ -1,7 +1,30 @@
-import React from "react"
+import React, { useContext} from "react"
+import { domainContext } from "../Context/Provider"
+import { Link } from "react-router-dom";
 
 export default function DomainDetails() {
+    const { domainName } = useContext(domainContext);
+    
     return (
-        <p>DomainDetails</p>
+        <main>
+            <Link to="/"><button>Voltar</button></Link>
+            <h1>Results for <b>{domainName}</b>:</h1>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>domain</td>
+                        <td>{domainName}</td>
+                    </tr>
+                    <tr>
+                        <td>host</td>
+                        <td>valor</td>
+                    </tr>
+                    <tr>
+                        <td>owner</td>
+                        <td>valor</td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
     )
 };
