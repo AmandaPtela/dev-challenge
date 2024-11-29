31ddb03ce634e7ad5e5fcaaa28b41afc6a8a0cf9
Amanda Portela <mandaptela@gmail.com>
2024-11-28 21:12:13 -0300

    feat: classes for styling purpose

--- src/Desafio.Umbler.Front/src/Components/DomainDetails.jsx
+++ src/Desafio.Umbler.Front/src/Components/DomainDetails.jsx
@@ -1,27 +1,29 @@
-import React, { useContext} from "react"
-import { domainContext } from "../Context/Provider"
-import { Link } from "react-router-dom";
+import React, { useContext } from "react"
+import { domainContext } from "../Context/Provider";
+
+import SearchDomain from "../Components/SearchDomain";
+import "../Styles/DomainDetails.css";
 
 export default function DomainDetails() {
     const { domainName } = useContext(domainContext);
-    
+
     return (
-        <main>
-            <Link to="/"><button>Voltar</button></Link>
+        <main id="mainDetailsPage">
+            <SearchDomain />
             <h1>Results for <b>{domainName}</b>:</h1>
             <table>
                 <tbody>
                     <tr>
-                        <td>domain</td>
-                        <td>{domainName}</td>
+                        <td className="key">domain:</td>
+                        <td className="value">{domainName}</td>
                     </tr>
                     <tr>
-                        <td>host</td>
-                        <td>valor</td>
+                        <td className="key">host:</td>
+                        <td className="value">valor</td>
                     </tr>
                     <tr>
-                        <td>owner</td>
-                        <td>valor</td>
+                        <td className="key">owner:</td>
+                        <td className="value">valor</td>
                     </tr>
                 </tbody>
             </table>
