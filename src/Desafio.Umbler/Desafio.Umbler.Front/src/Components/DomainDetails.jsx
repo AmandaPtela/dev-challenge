8a7f8f74baec95132ac9b7887747d02264371770
Amanda Portela <mandaptela@gmail.com>
2024-12-03 20:35:28 -0300

    feat: initial structure of function for formating domain's "whoIs" data

--- src/Desafio.Umbler/Desafio.Umbler.Front/src/Components/DomainDetails.jsx
+++ src/Desafio.Umbler/Desafio.Umbler.Front/src/Components/DomainDetails.jsx
@@ -1,44 +1,67 @@
 import React, { useContext, useEffect, useState } from "react"
 import { domainContext } from "../Context/Provider";
-import { getDomain } from "../Service/api";
+
 
 import SearchDomain from "../Components/SearchDomain";
 import "../Styles/DomainDetails.css";
 
 export default function DomainDetails() {
     const { domainName } = useContext(domainContext);
-    let [data, setData] = useState([])
+    let [data, setData] = useState([]);
+    let [whoIs, setWhoIs] = useState([]);
+    let [whoIsData, setWhoIsData] = useState([]);
+    let [loadingToRender, setLoadingToRender] = useState(true);
     
     useEffect(() => {
         fetch(`/api/domain/${domainName}`)
             .then(response => response.json())
-            .then(data => setData(data))
-            .catch(error => console.error("Erro ao buscar dados do domínio:", error));
+            .then(data => {
+                setData(data)
+                setWhoIs(Object.entries(data)[4].find((e) => e !== "whoIs").split("\n"));
+            })
+            .catch(error => console.error("Erro ao buscar dados do domínio :", error));
     }, [domainName]);
+    
+    function setWhoIsDetails() {
+        const keyList = [];
+        const valueList = [];
+        const whoIsData = {};
 
-    const chaves = Object.entries(data).filter((e) => !e.includes("whoIs"));
-    const whoIs = Object.entries(data).filter((e) => e.includes("whoIs"));
+        whoIs.map((element) => {
+            let whoIsKey = element.split(":")[0].split(" ").join("_") ;
+            let whoIsValue = element.split(":")[1];
+            
+            keyList.push(whoIsKey);
+            valueList.push(whoIsValue);
+            
+            whoIsData[whoIsKey] = whoIsValue
+            setWhoIsData(whoIsData);
+            return console.log(whoIsData);
+            
+        });
+    };
 
     return (
         <main id="mainDetailsPage">
             <SearchDomain />
-            <h1>Results for <b>{domainName}</b>:</h1>
+            <h1>Results for <b>{data.name}</b>:</h1>
             <table>
                 <tbody>
                     <tr>
-                        <td className="key">domain:</td>
-                        <td className="value">{domainName}</td>
+                        <td className="key">Domain:</td>
+                        <td className="value">{data.name}</td>
                     </tr>
                     <tr>
-                        <td className="key">host:</td>
-                        <td className="value">valor</td>
+                        <td className="key">IP:</td>
+                        <td className="value">{data.ip}</td>
                     </tr>
                     <tr>
-                        <td className="key">owner:</td>
-                        <td className="value">valor</td>
+                        <td className="key">WhoIs:</td>
+                        <td className="value">asa</td>
                     </tr>
                 </tbody>
             </table>
+            <p>WhoIs: </p>
         </main>
     )
 };
