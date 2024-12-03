1fb55a834fda26be9c178fdc9e20fd704e035f75
Amanda Portela <mandaptela@gmail.com>
2024-12-02 21:05:39 -0300

    ref: simple api with axios

--- src/Desafio.Umbler/Desafio.Umbler.Front/src/Service/api.js
+++ src/Desafio.Umbler/Desafio.Umbler.Front/src/Service/api.js
@@ -1,40 +1,13 @@
-const Request = window.Request
-const Headers = window.Headers
-const fetch = window.fetch
+import axios from "axios";
 
-class Api {
-  async request (method, url, body) {
-    if (body) {
-      body = JSON.stringify(body)
-    }
+const api = axios.create({
+  baseURL: "http://localhost:65453/",
+});
 
-    const request = new Request('/api/' + url, {
-      method: method,
-      body: body,
-      credentials: 'same-origin',
-      headers: new Headers({
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      })
-    })
-
-    const resp = await fetch(request)
-    if (!resp.ok && resp.status !== 400) {
-      throw Error(resp.statusText)
-    }
-
-    const jsonResult = await resp.json()
-
-    if (resp.status === 400) {
-      jsonResult.requestStatus = 400
-    }
-
-    return jsonResult
-  }
-
-  async getDomain (domainOrIp) {
-    console.log( await this.request('GET', `domain/${domainOrIp}`));
-  }
+export async function getDomain(domain) {
+  const response = await api.get(domain);
+  console.log("ENTROU");
+    console.log(response.status);
+    
+    return response;
 }
-
-export const api = new Api();
