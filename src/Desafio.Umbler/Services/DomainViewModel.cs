3857d1841e0684dd5e1b7a4bb0bfd192eca5fbb1
Amanda Portela <mandaptela@gmail.com>
2024-12-15 20:10:33 -0300

    ref: validation function moved to repository

--- src/Desafio.Umbler/Services/DomainViewModel.cs
+++ src/Desafio.Umbler/Services/DomainViewModel.cs
@@ -1,22 +1,9 @@
 using System;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
 namespace Desafio.Umbler.ViewModel
 {
     public class DomainViewModel
     {
-        public bool DomainValidation(string domain)
-        {
-            string pattern = @"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.(?!-)[A-Za-z0-9-]{2,63}(?<!-)$";
-            bool valid = Regex.IsMatch(domain, pattern);
-
-            return valid;
-        }
-
         public string Name { get; set; }
         public string Ip { get; set; }
         public string WhoIs { get; set; }
