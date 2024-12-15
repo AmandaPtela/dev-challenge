2187d330f01a7c878edc76749cddd73745da7a03
Amanda Portela <mandaptela@gmail.com>
2024-12-15 18:48:27 -0300

    ref: directly use of repository

--- src/Desafio.Umbler/Controllers/DomainController.cs
+++ src/Desafio.Umbler/Controllers/DomainController.cs
@@ -2,7 +2,10 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Desafio.Umbler.ViewModel;
+using Desafio.Umbler.Models;
+using Desafio.Umbler.DatabaseRepo;
 
 namespace Desafio.Umbler.Controllers
 {
@@ -10,13 +13,42 @@ namespace Desafio.Umbler.Controllers
     public class DomainController : Controller
     {
         public DomainViewModel DomainViewModel = new DomainViewModel();
+        private readonly DatabaseContext _db;
+        public DomainController(DatabaseContext db)
+        {
+            _db = db;
+        }
 
         [HttpGet, Route("domain/{domainName}")]
-
         public async Task<IActionResult> Get(string domainName)
         {
-            var result = await DomainViewModel.DomainRequest(domainName);
+            var _DatabaseRepository = new DatabaseRepository(_db);
+            var domain = await _DatabaseRepository.GetDomain(domainName);
 
-           return Ok(result);
+            if (domain == null)
+            {
+                return await _DatabaseRepository.AddDomain(domainName);
+            }
+
+            if (DateTime.Now.Subtract(domain.UpdatedAt).TotalMinutes > domain.Ttl)
+            {
+                return await _DatabaseRepository.VerifyTtl(domainName);
+            }
+
+            var ViewModelReturn = new DomainViewModel
+            {
+                Name = domain.Name,
+                Ip = domain.Ip,
+                WhoIs = domain.WhoIs,
+                HostedAt = domain.HostedAt,
+
+            };
+
+            Console.WriteLine(ViewModelReturn);
+
+            await _DatabaseRepository.SaveChangesAsync();
+
+            return Ok(ViewModelReturn);
+        }
     }
-}}
+}
