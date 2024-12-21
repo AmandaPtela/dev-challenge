5362f8e51a36a4ecc6004abac725aedb5abf4b1a
Amanda Portela <mandaptela@gmail.com>
2024-12-21 13:16:41 -0300

    ref: default value in case message comes null

--- src/Desafio.Umbler/Desafio.Umbler.Front/src/Pages/Error.jsx
+++ src/Desafio.Umbler/Desafio.Umbler.Front/src/Pages/Error.jsx
@@ -4,7 +4,7 @@ export default function Error() {
   const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     
-    const Message = searchParams.get('Message');// || 'Ocorreu um erro inesperado.';
+    const Message = searchParams.get('Message') || 'Ocorreu um erro';
     console.log(Message);
   return (<h1>{Message}</h1>);
 }
