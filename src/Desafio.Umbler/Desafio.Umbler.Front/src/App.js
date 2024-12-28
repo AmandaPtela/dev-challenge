ee49a33863fbb0effbd5542422e02e658b7d2784
Amanda Portela <mandaptela@gmail.com>
2024-12-28 14:43:22 -0300

    fix: route

--- src/Desafio.Umbler/Desafio.Umbler.Front/src/App.js
+++ src/Desafio.Umbler/Desafio.Umbler.Front/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route exact path="/" Component={Home} />
           <Route path="api/domain/:domain" Component={DomainDetails} />
-          <Route path="/error" Component={ErrorPage} />
+          <Route path="/Error/Error" Component={ErrorPage} />
         </Routes>
       </BrowserRouter>        
     </div>
