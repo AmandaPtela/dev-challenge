e07c700487a443dca46ad16c69c0890c8f47feae
Amanda Portela <mandaptela@gmail.com>
2024-12-28 20:52:42 -0300

    ref: error now is handled on domainsDetails

--- src/Desafio.Umbler/Desafio.Umbler.Front/src/Components/SearchDomain.jsx
+++ src/Desafio.Umbler/Desafio.Umbler.Front/src/Components/SearchDomain.jsx
@@ -1,56 +1,57 @@
 import React, { useState, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { domainContext } from "../Context/Provider";
 
 import "../Styles/SearchDomain.css";
 
 export default function Search() {
-    let { setDomainName } = useContext(domainContext);
-    const [domain, setDomain] = useState("");
-    let [valid, setValid] = useState(true);
+  let { setDomainName } = useContext(domainContext);
+  const [domain, setDomain] = useState("");
+  let [valid, setValid] = useState(true);
 
-    const INPUT_MAX_LENGTH = 50;
-    const navigate = useNavigate();
+  const INPUT_MAX_LENGTH = 50;
+  const navigate = useNavigate();
 
-    function handleValue(value) {
-        const regex = /^(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.(?!-)[A-Za-z0-9-]{2,63}(?<!-)$/
+  function handleValue(value) {
+    const regex = /^(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.(?!-)[A-Za-z0-9-]{2,63}(?<!-)$/;
+    
+    if (!value.length === 0 || !regex.test(value)) {
+      return setValid(false);
+    };
+    setDomain(value);
+    return setValid(true);
+  }
 
-        if (!value.length === 0 || !regex.test(value)) {
-            return setValid(false);
-        };
-        setValid(true);
-        setDomain(value);
-    }
+  function searchInfo() {
+    if (valid) {
+      setDomainName(domain);
+      return navigate(`api/domain/${domain}`);
+    };
+    return ;
+  }
 
-    function searchInfo() {
-        if (valid) {
-            setDomainName(domain);
-            
-            return navigate(`api/domain/${domain}`);
-        };
-        throw new Error("Domínio Inválido");
-    }
-
-    return (
-        <main id="mainSearchComponent">
-            <div id="searchArea">
-                <input
-                    type="text"
-                    onChange={({ target }) => handleValue(target.value)}
-                    id="searchInput"
-                    maxLength={INPUT_MAX_LENGTH}
-                />
-                <button
-                    type="submit"
-                    id="searchButton"
-                    onClick={() => searchInfo()}
-                >
-                    Buscar
-                </button>
-            </div>
-            <span id="inputInformation" >
-                {!valid && "Domínio inválido"}
-            </span>
-        </main >
-    )
-};
+  return (
+    <main id="mainSearchComponent">
+      <div id="searchArea">
+        <input
+          type="text"
+          onChange={({ target }) => handleValue(target.value)}
+          id="searchInput"
+          maxLength={INPUT_MAX_LENGTH}
+        />
+        <button type="submit" id="searchButton" onClick={() => searchInfo()}>
+          Buscar
+        </button>
+      </div>
+      <span id="inputInformation">
+        {!valid && "Domínio inválido"}
+      </span>
+        {/* {data ? (
+            <div>
+            <DomainDetails />
+          </div>
+        )
+    : (null)} */}
+    </main>
+  );
+}
