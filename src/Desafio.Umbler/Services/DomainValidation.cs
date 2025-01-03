d29478aa358c701f1782c999024c84dda4afb306
Amanda Portela <mandaptela@gmail.com>
2025-01-03 12:47:54 -0300

    fix: domain's minimum length adjusted to include domain's extension

--- src/Desafio.Umbler/Services/DomainValidation.cs
+++ src/Desafio.Umbler/Services/DomainValidation.cs
@@ -6,14 +6,15 @@ namespace Desafio.Umbler.Validator
 {
     public class DomainValidator : AbstractValidator<string>
     {
-        public DomainValidator() {
+        public DomainValidator()
+        {
             RuleFor(domainName => domainName)
             .MaximumLength(30).WithMessage("O domínio deve conter no máximo 30 caracteres");
             RuleFor(domainName => domainName)
-            .MinimumLength(2).WithMessage("O domínio deve conter no mínimo 2 caracteres");
+            .MinimumLength(6).WithMessage("O domínio deve conter no mínimo 6 caracteres");
             RuleFor(domainName => domainName)
             .Matches(@"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.(?!-)[A-Za-z0-9-]{2,63}(?<!-)$")
             .WithMessage("Formato de domínio Inválido ou domínio ausente");
-        }   
+        }
     }
 }
