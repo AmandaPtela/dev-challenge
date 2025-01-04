47971460482407d611a57f4c58391a4cdba0117f
Amanda Portela <mandaptela@gmail.com>
2025-01-04 12:01:20 -0300

    feat: styling button import

--- src/Desafio.Umbler/Desafio.Umbler.Front/src/Components/BackButton.jsx
+++ src/Desafio.Umbler/Desafio.Umbler.Front/src/Components/BackButton.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "../Styles/Buttons.css";
 
 export default function BackButton({route}) {
     const navigate = useNavigate();
     return(
       <button
       type="button"
+      id= "backButton"
       onClick={() => navigate(route)}
       >
     Back
