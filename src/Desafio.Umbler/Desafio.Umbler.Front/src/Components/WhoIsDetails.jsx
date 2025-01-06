dde07aac45783252d74f06bb1cf0ec92af31d42c
Amanda Portela <mandaptela@gmail.com>
2025-01-06 13:15:33 -0300

    feat: card for whois details

---
+++ src/Desafio.Umbler/Desafio.Umbler.Front/src/Components/WhoIsDetails.jsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: "rotate(0deg)",
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: "rotate(180deg)",
+      },
+    },
+  ],
+}));
+
+export default function WhoIsDetails() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card
+      style={{
+        width: "70%",
+        minHeight: "10%",
+        backgroundColor: "transparent",
+        flexDirection: "row",
+      }}
+    >
+      <CardContent>
+        <Typography
+          variant="body2"
+          sx={{
+            color: "#343F3E;",
+            fontSize: "larger",
+            display: "flex",
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center"
+          }}
+        >
+          WhoIs info
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </Typography>
+      </CardContent>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography sx={{ marginBottom: 2 }}>ASADAWDAWDAWDX ADAWDAW</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
