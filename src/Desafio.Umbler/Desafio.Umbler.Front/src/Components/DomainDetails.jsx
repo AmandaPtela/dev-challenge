39cc1619b2dae33df3788f373d2edfc571bdf893
Amanda Portela <mandaptela@gmail.com>
2025-01-07 23:06:59 -0300

    ref: variables and function to set whois's data removed

--- src/Desafio.Umbler/Desafio.Umbler.Front/src/Components/DomainDetails.jsx
+++ src/Desafio.Umbler/Desafio.Umbler.Front/src/Components/DomainDetails.jsx
@@ -2,49 +2,27 @@ import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { domainContext } from "../Context/Provider";
 
-import SearchDomain from "../Components/SearchDomain";
 import "../Styles/DomainDetails.css";
 import BackButton from "./BackButton";
 
 export default function DomainDetails() {
   const { domainName, setErrorDetails } = useContext(domainContext);
   let [data, setData] = useState([]);
-  let [whoIs, setWhoIs] = useState([]);
-  let [whoIsData, setWhoIsData] = useState([]);
-  let [loadingToRender, setLoadingToRender] = useState(true);
   const navigate = useNavigate();
 
   useEffect( () => {
-      const fetcha = async () => await fetch(`/api/domain/${domainName}`)
-      .then((response) => response.json())
-      .then((data) => {
+    const fetcha = async () => await fetch(`/api/domain/${domainName}`)
+        .then((response) => response.json())
+        .then((data) => {
           setData(data);
-          //setWhoIs(Object.entries(data)[4].find((e) => e !== "whoIs").split("\n"));
           if(data.redirectUrl) {
             setErrorDetails(data.redirectUrl);
             return navigate("/Error/Error");
           }
-        })
-      fetcha();
+        });
+
+    fetcha();
   }, [domainName]);
-
-  function setWhoIsDetails() {
-    const keyList = [];
-    const valueList = [];
-    const whoIsData = {};
-
-    whoIs.map((element) => {
-      let whoIsKey = element.split(":")[0].split(" ").join("_");
-      let whoIsValue = element.split(":")[1];
-
-      keyList.push(whoIsKey);
-      valueList.push(whoIsValue);
-
-      whoIsData[whoIsKey] = whoIsValue;
-      setWhoIsData(whoIsData);
-      return console.log(whoIsData);
-    });
-  }
 
   return (
     <main id="mainDetailsPage">
